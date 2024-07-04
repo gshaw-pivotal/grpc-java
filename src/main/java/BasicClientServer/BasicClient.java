8a966633be56a6b18bf83a7cfd1e219aeaf2488f
Gavin Shaw <gavshaw+github@gmail.com>
2024-07-04 16:00:44 -0400

    Changed the client to use a non blocking stub
    - Did just for experimentation, not share if it provides any advantages
      in such a basic demo.

--- src/main/java/BasicClientServer/BasicClient.java
+++ src/main/java/BasicClientServer/BasicClient.java
@@ -1,9 +1,11 @@
 package BasicClientServer;
 
+import coms.StandardReply;
 import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
@@ -11,12 +13,12 @@ import java.util.logging.Logger;
 public class BasicClient {
     private static final Logger logger = Logger.getLogger(BasicClient.class.getName());
 
-    private final coms.ComServiceGrpc.ComServiceBlockingStub blockingStub;
+    private final coms.ComServiceGrpc.ComServiceStub nonBlockingStub;
 
     private static final String serverAddress = "localhost:50501";
 
     public BasicClient(Channel channel) {
-        blockingStub = coms.ComServiceGrpc.newBlockingStub(channel);
+        nonBlockingStub = coms.ComServiceGrpc.newStub(channel);
     }
 
     public static void main(String[] args) throws Exception {
@@ -40,16 +42,34 @@ public class BasicClient {
         }
     }
 
+    public StreamObserver<coms.StandardReply> getServerResponseObserver() {
+        StreamObserver<coms.StandardReply> observer = new StreamObserver<StandardReply>() {
+            @Override
+            public void onNext(StandardReply standardReply) {
+                logger.info("Client received: " + standardReply.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+        };
+
+        return observer;
+    }
+
     private void sendMessage(String message) {
         coms.StandardRequest request = coms.StandardRequest.newBuilder().setMessage(message).build();
 
         coms.StandardReply reply;
 
         try {
-            reply = blockingStub.standardCom(request);
+            nonBlockingStub.standardCom(request, getServerResponseObserver());
         } catch (StatusRuntimeException e) {
             return;
         }
-        logger.info("Client received: " + reply.getMessage());
     }
 }
